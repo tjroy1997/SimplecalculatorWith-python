a3f89bb6c315eaaee0132ae11594824d40bba06a
Tamal Joyti Roy <46474086+tjroy1997@users.noreply.github.com>
2020-08-11 13:01:24 +0600

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+def sum(num1,num2):
+    num1=int(input("enter value 1\n"))
+    num2 = int(input("enter value 2\n"))
+    sum=num1+num2
+    print("summation is",sum)
+def mul(num1,num2):
+    num1 = int(input("enter value 1\n"))
+    num2 = int(input("enter value 2\n"))
+    mul=num1*num2
+    print("Multiplication is ",mul)
+def div(num1,num2):
+    num1 = int(input("enter value 1\n"))
+    num2 = int(input("enter value 2\n"))
+    if num1>num2:
+        div=num2/num1
+    else:
+        div=num1/num1
+    print("Division is ",div)
+
+def sub(num1,num2):
+    num1 = int(input("enter value 1\n"))
+    num2 = int(input("enter value 2\n"))
+    sub=num2-num1
+    print("Substraction is ",sub)
+
+print("Now printing the values\n")
+sum(12,34)
+mul(2,3)
+div(45,23)
+sub(23,2)
